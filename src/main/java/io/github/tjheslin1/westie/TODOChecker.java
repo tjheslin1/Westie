80b65e3b144734a0ef83151fb0679e099a1185e9
Thomas Heslin <tjheslin1@gmail.com>
2016-10-19 21:42:43 +0100

    Regenerated project.

---
+++ src/main/java/io/github/tjheslin1/westie/TODOChecker.java
@@ -0,0 +1,6 @@
+package io.github.tjheslin1.westie;
+
+public class TODOChecker {
+
+    private static final String TODO_REGEX = "\\/\\/[ ]*TODO.*";
+}
---
+++ src/test/java/io/github/tjheslin1/westie/TODOCheckerTest.java
@@ -0,0 +1,7 @@
+package io.github.tjheslin1.westie;
+
+import org.assertj.core.api.WithAssertions;
+
+public class TODOCheckerTest implements WithAssertions {
+
+}
