bbc9a3bb6ae4499933cb7c13fb06fd891770eb7f
Thomas Heslin <tjheslin1@gmail.com>
2016-11-23 21:31:16 +0000

    Adding javadoc to WestieRegexes

--- src/main/java/io/github/tjheslin1/westie/WestieRegexes.java
+++ src/main/java/io/github/tjheslin1/westie/WestieRegexes.java
@@ -1,5 +1,25 @@
+/*
+ * Copyright 2016 Thomas Heslin <tjheslin1@gmail.com>.
+ *
+ * This file is part of Westie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+* limitations under the License.
+ */
 package io.github.tjheslin1.westie;
 
+/**
+ * Reusable regexes for writing project-specific static analysis checkers.
+ */
 public class WestieRegexes {
 
     public final static String TODOS_MUST_HAVE_DATE_REGEX = ".*//[ ]*TODO.*[0-9]{1,4}[/-]{1}[A-z0-9]{2,3}[/-]{1}[0-9]{1,4}.*";
