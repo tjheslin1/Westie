bd3af46e921a94cb32eb13b3f800866d6cc0680f
Thomas Heslin <tjheslin1@gmail.com>
2016-11-23 21:19:14 +0000

    Added test to check JiraReferenceCheckerTest excluded files

--- src/test/java/io/github/tjheslin1/westie/jiraallowedstatus/JiraReferenceCheckerTest.java
+++ src/test/java/io/github/tjheslin1/westie/jiraallowedstatus/JiraReferenceCheckerTest.java
@@ -6,11 +6,11 @@ import io.github.tjheslin1.westie.infrastructure.JiraIssues;
 import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 
 public class JiraReferenceCheckerTest implements WithAssertions, WithMockito {
 
@@ -31,5 +31,17 @@ public class JiraReferenceCheckerTest implements WithAssertions, WithMockito {
         assertThat(violations.size()).isEqualTo(2);
         assertThat(violations.get(0).toString()).matches("Line '//TODO MON-100 make this final' in file '.*/io/github/tjheslin1/examples/jira/many/packages/ClassWithJiraTodos.java.*");
         assertThat(violations.get(1).toString()).matches("Line '// TODO MON-101 set passed parameter as name' in file '.*/io/github/tjheslin1/examples/jira/many/packages/ClassWithJiraTodos.java.*");
+    }
+
+    @Test
+    public void doesNotReportOnIgnoredFiles() throws Exception {
+        when(jiraIssues.isJiraIssueInAllowedStatus(ISSUE_NUMBER)).thenReturn(false);
+        when(jiraIssues.allowedStatuses()).thenReturn(asList("Ready To Play", "Development"));
+
+        JiraReferenceChecker jiraReferenceChecker = new JiraReferenceChecker(jiraIssues, JIRA_ISSUE_REGEX, singletonList("ClassWithJiraTodos.java"));
+
+        List<Violation> violations = jiraReferenceChecker.checkAllJiraTodosAreInAllowedStatuses(Paths.get("src/test/resources/io/github/tjheslin1/examples/jira"));
+
+        assertThat(violations).isEmpty();
     }
 }
