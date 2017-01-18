c1d5b9ce5687b126d068bf87bccb922e5ece9760
Thomas Heslin <tjheslin1@gmail.com>
2017-01-18 22:07:55 +0000

    Added KataCalisthenicsEnforcer class skeletons.

---
+++ src/main/java/io/github/tjheslin1/westie/KataCalisthenicsEnforcer/KataCalisthenicsEnforcer.java
@@ -0,0 +1,5 @@
+package io.github.tjheslin1.westie.KataCalisthenicsEnforcer;
+
+public class KataCalisthenicsEnforcer {
+
+}
---
+++ src/test/java/io/github/tjheslin1/westie/KataCalisthenicsEnforcer/KataCalisthenicsEnforcerTest.java
@@ -0,0 +1,8 @@
+package io.github.tjheslin1.westie.KataCalisthenicsEnforcer;
+
+import org.assertj.core.api.WithAssertions;
+
+public class KataCalisthenicsEnforcerTest implements WithAssertions {
+
+
+}
