09fdaddc86724e0d97d9d1a4bd9c63b8fc107f14
Thomas Heslin <tjheslin1@gmail.com>
2017-08-02 15:59:40 +0100

    Removing Kata calisthenics checker. Will re-add if requested.

--- src/test/java/io/github/tjheslin1/westie/katacalisthenicsenforcer/KataCalisthenicsAnalyserTest.java
+++
@@ -1,8 +0,0 @@
-package io.github.tjheslin1.westie.katacalisthenicsenforcer;
-
-import org.assertj.core.api.WithAssertions;
-
-public class KataCalisthenicsAnalyserTest implements WithAssertions {
-
-    
-}
